cc70444293c50cfb84137795a650ca19e1d3849c
masudurrahaman1 <masudurrahamanrm@gmail.com>
2026-05-01 07:27:30 +0530

    feat: add smooth page transition animations using framer-motion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -10,8 +11,11 @@ import TeamPage from './pages/TeamPage';
 import ContactPage from './pages/ContactPage';
 import ScrollToTop from './components/ScrollToTop';
 import MobileNav from './components/MobileNav';
+import PageTransition from './components/PageTransition';
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <ScrollToTop />
@@ -19,14 +23,16 @@ function App() {
         <Navbar />
 
         <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<ServicesPage />} />
-            <Route path="/portfolio" element={<PortfolioPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/team" element={<TeamPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-          </Routes>
+          <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+              <Route path="/" element={<PageTransition><Home /></PageTransition>} />
+              <Route path="/services" element={<PageTransition><ServicesPage /></PageTransition>} />
+              <Route path="/portfolio" element={<PageTransition><PortfolioPage /></PageTransition>} />
+              <Route path="/about" element={<PageTransition><AboutPage /></PageTransition>} />
+              <Route path="/team" element={<PageTransition><TeamPage /></PageTransition>} />
+              <Route path="/contact" element={<PageTransition><ContactPage /></PageTransition>} />
+            </Routes>
+          </AnimatePresence>
         </main>
 
         <MobileNav />
